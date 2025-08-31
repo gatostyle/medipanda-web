a98708c4686845cf433d0f6710d3aa8b35a85175
JoonYoung Lee <kuroneko0441@gmail.com>
2025-08-31 20:16:42 +0900

    타입헬퍼

--- src/lib/type-helpers.d.ts
+++ src/lib/type-helpers.d.ts
@@ -1,3 +1,5 @@
 interface ObjectConstructor {
   keys<T extends object>(o: T): (keyof T)[];
+  values<T extends object>(o: T): T[keyof T][];
+  entries<T extends object>(o: T): [keyof T, T[keyof T]][];
 }
