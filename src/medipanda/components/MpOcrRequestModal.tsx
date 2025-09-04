58c1a77c2f0e90e10a8b8a32561eb0e5ba7f97d8
JoonYoung Lee <kuroneko0441@gmail.com>
2025-09-04 18:17:23 +0900

    파일 이동

--- src/medipanda/components/MpOcrRequestModal.tsx
+++ src/medipanda/components/MpOcrRequestModal.tsx
@@ -2,7 +2,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, LinearProgress, Typography } from '@mui/material';
-import { OcrResponse, requestOcr } from '@/backend';
+import { OcrResponse, requestOcr } from '@/ocr';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 interface Point {
