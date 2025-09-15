2c830c45f6549154fccc19d5c4ec0a602dabad3e
JoonYoung Lee <kuroneko0441@gmail.com>
2025-09-15 17:48:55 +0900

    로그인 페이지에서 <Grid /> 컴포넌트 제거

--- src/medipanda/pages/MpLogin.tsx
+++ src/medipanda/pages/MpLogin.tsx
@@ -1,4 +1,4 @@
-import { Button, FormHelperText, Grid, InputAdornment, Stack } from '@mui/material';
+import { Button, FormHelperText, InputAdornment, Stack } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { isAxiosError } from 'axios';
 import IconButton from 'components/@extended/IconButton';
@@ -73,66 +73,50 @@ export default function MpLogin() {
 
   return (
     <AuthWrapper>
-      <Grid container spacing={3} component='form' noValidate onSubmit={handleSubmit}>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          <h3>로그인</h3>
-        </Grid>
-        <Grid item xs={12}>
-          <Stack spacing={1}>
-            <TextField
-              type='userId'
-              value={values.userId}
-              name='userId'
-              onBlur={handleBlur}
-              onChange={handleChange}
-              label='이메일'
-              fullWidth
-              error={Boolean(touched.userId && errors.userId)}
-            />
-          </Stack>
-          {touched.userId && errors.userId && <FormHelperText error>{errors.userId}</FormHelperText>}
-        </Grid>
-        <Grid item xs={12}>
-          <Stack spacing={1}>
-            <TextField
-              fullWidth
-              error={Boolean(touched.password && errors.password)}
-              type={showPassword ? 'text' : 'password'}
-              value={values.password}
-              name='password'
-              onBlur={handleBlur}
-              onChange={handleChange}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge='end'
-                      color='secondary'
-                    >
-                      {showPassword ? <Eye /> : <EyeSlash />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              label='비밀번호'
-            />
-          </Stack>
-          {touched.password && errors.password && <FormHelperText error>{errors.password}</FormHelperText>}
-        </Grid>
-        {errors.submit && (
-          <Grid item xs={12}>
-            <FormHelperText error>{errors.submit}</FormHelperText>
-          </Grid>
-        )}
-        <Grid item xs={12} sx={{ pointerEvents: isSubmitting ? 'none' : undefined }}>
-          <Button disabled={isSubmitting} fullWidth size='large' type='submit' variant='contained' color='primary'>
-            로그인
-          </Button>
-        </Grid>
-      </Grid>
+      <Stack component='form' noValidate onSubmit={handleSubmit} sx={{ width: '415px', gap: 3 }}>
+        <h3 style={{ textAlign: 'center' }}>로그인</h3>
+        <TextField
+          type='userId'
+          value={values.userId}
+          name='userId'
+          onBlur={handleBlur}
+          onChange={handleChange}
+          label='이메일'
+          fullWidth
+          error={Boolean(touched.userId && errors.userId)}
+          helperText={touched.userId ? errors.userId : ''}
+        />
+        <TextField
+          fullWidth
+          type={showPassword ? 'text' : 'password'}
+          value={values.password}
+          name='password'
+          onBlur={handleBlur}
+          onChange={handleChange}
+          error={Boolean(touched.password && errors.password)}
+          helperText={touched.password ? errors.password : ''}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                  color='secondary'
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          label='비밀번호'
+        />
+        {errors.submit && <FormHelperText error>{errors.submit}</FormHelperText>}
+        <Button disabled={isSubmitting} fullWidth size='large' type='submit' variant='contained' color='primary'>
+          로그인
+        </Button>
+      </Stack>
     </AuthWrapper>
   );
 }
