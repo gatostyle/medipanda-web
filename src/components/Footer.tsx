5194cad36fd1ae7cf3b1e4e5b8f3bf5d7203b4a8
JoonYoung Lee <kuroneko0441@gmail.com>
2025-10-12 05:34:25 +0900

    푸터 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -59,14 +59,23 @@ export function Footer() {
               </Link>
             </Stack>
             <Typography variant='smallTextR' sx={{ color: colors.white }}>
-              법인명 : (주)케이앤메디슨 | 서울시 강남구 논현로 416, 4층 운기빌딩(역삼동) | 대표 : 황혁진 | 사업자등록번호 : 338-81-00767
+              법인명: (주)케이앤메디슨 | 서울특별시 강남구 논현로 416, 지하1층 운기빌딩(역삼동) | 대표 황혁진 | 사업자등록번호 465-86-03299
               <br />
-              팩스 : 02-6280-6393 | 이메일 : keymedi@keymedi.com
+              대표전화: 070-7599-2528
               <br />
-              대표전화 : 02-540-0703 | 고객센터 : 오전 10시~오후 5시(토/일/공휴일 휴무)
+              이메일:{' '}
+              <Link
+                underline='hover'
+                component={RouterLink}
+                to='mailto:info@knmedicine.com'
+                sx={{
+                  color: colors.white,
+                }}
+              >
+                info@knmedicine.com
+              </Link>
               <br />
-              통신판매번호 : 제2017-서울강남-03514호 | 개인정보관리책임자 : 강승균 | 메일 : keymedi@keymedi.com | Copyright©keymedi All
-              Rights Reserved.
+              통신판매번호: 제 2025-서울강남-05488 호
             </Typography>
           </Stack>
         </Stack>
