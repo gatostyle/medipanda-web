bf6243686079775793f17f4bc97f471231a1c4ee
JoonYoung Lee <kuroneko0441@gmail.com>
2025-10-12 19:52:16 +0900

    한번에 업로드 기능 깔끔하게 제거

--- src/pages/PrescriptionList.tsx
+++ src/pages/PrescriptionList.tsx
@@ -2,13 +2,11 @@ import {
   DateTimeString,
   type DealerResponse,
   type DrugCompanyResponse,
-  type FileValidationErrorDto,
   type PartnerResponse,
   type PrescriptionResponse,
   PrescriptionStatusLabel,
   PrescriptionTypeLabel,
   searchPrescriptions,
-  uploadEdiZip,
   uploadPartnerEdiFiles,
 } from '@/backend';
 import { DealerSelectDialog } from '@/custom/components/DealerSelectDialog';
@@ -20,25 +18,12 @@ import { MedipandaOutlinedInput } from '@/custom/components/MedipandaOutlinedInp
 import { MedipandaPagination } from '@/custom/components/MedipandaPagination';
 import { MedipandaTable } from '@/custom/components/MedipandaTable';
 import { PartnerSelectDialog } from '@/custom/components/PartnerSelectDialog';
-import { useSession } from '@/hooks/useSession';
 import { useSearchParamsOrDefault } from '@/lib/hooks/useSearchParamsOrDefault';
 import { setUrlParams } from '@/lib/utils/url';
 import { colors } from '@/themes';
 import { DATEFORMAT_YYYY_MM, DATEFORMAT_YYYY년_MM월 } from '@/lib/utils/dateFormat';
 import { Search } from '@mui/icons-material';
-import {
-  Dialog,
-  DialogTitle,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  MenuItem,
-  PaginationItem,
-  Select,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { FormControl, IconButton, InputAdornment, MenuItem, PaginationItem, Select, Stack, TextField, Typography } from '@mui/material';
 import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
@@ -103,8 +88,6 @@ export default function PrescriptionList() {
     form.setValue('searchKeyword', searchKeyword);
     fetchContents();
   }, [searchType, searchKeyword, page]);
-
-  const [individualUpload, setIndividualUpload] = useState(true);
 
   const table = useReactTable({
     data: contents,
@@ -185,20 +168,7 @@ export default function PrescriptionList() {
         />
         <button type='submit' hidden />
       </Stack>
-      <Stack direction='row' alignItems='center' gap='10px' sx={{ marginTop: '40px' }}>
-        <MedipandaButton
-          variant={individualUpload ? 'contained' : 'outlined'}
-          rounded
-          onClick={() => setIndividualUpload(true)}
-          sx={{
-            width: '140px',
-            marginLeft: 'auto',
-          }}
-        >
-          거래처별 업로드
-        </MedipandaButton>
-      </Stack>
-      <Stack direction='row' alignItems='flex-start' gap='24px' sx={{ marginTop: '10px' }}>
+      <Stack direction='row' alignItems='flex-start' gap='24px' sx={{ marginTop: '40px' }}>
         <Stack sx={{ width: '600px' }}>
           <MedipandaTable table={table} />
           <MedipandaPagination
@@ -224,7 +194,7 @@ export default function PrescriptionList() {
             boxSizing: 'border-box',
           }}
         >
-          {individualUpload ? <EdiIndividualUploadForm /> : <EdiBatchUploadForm />}
+          <EdiIndividualUploadForm />
         </Stack>
       </Stack>
     </>
@@ -546,255 +516,5 @@ function EdiIndividualUploadForm() {
         )}
       />
     </>
-  );
-}
-
-function EdiBatchUploadForm() {
-  const { session } = useSession();
-  const [uploadErrors, setUploadErrors] = useState<FileValidationErrorDto[]>([]);
-
-  const form = useForm({
-    defaultValues: {
-      settlementMonth: null as Date | null,
-      prescriptionMonth: null as Date | null,
-      file: null as File | null,
-    },
-  });
-
-  const submitHandler: SubmitHandler<RequiredDeep<(typeof form)['control']['_defaultValues']>> = async values => {
-    if (values.settlementMonth == null) {
-      alert('정산월을 선택해 주세요.');
-      return;
-    }
-
-    if (values.prescriptionMonth == null) {
-      alert('처방월을 선택해 주세요.');
-      return;
-    }
-
-    if (values.file == null) {
-      alert('EDI 파일을 선택해 주세요.');
-      return;
-    }
-
-    try {
-      const result = await uploadEdiZip({
-        partnerUserId: session!.userId,
-        settlementMonth: format(values.prescriptionMonth!, DATEFORMAT_YYYY_MM),
-        prescriptionMonth: format(values.prescriptionMonth!, DATEFORMAT_YYYY_MM),
-        file: values.file!,
-      });
-
-      if (result.success) {
-        alert('EDI 파일이 성공적으로 업로드되었습니다.');
-        form.reset();
-      } else {
-        setUploadErrors(result.errors);
-      }
-    } catch (error) {
-      console.error('Error uploading EDI zip file:', error);
-      alert('EDI 파일 업로드 중 오류가 발생했습니다. 다시 시도해 주세요.');
-    }
-  };
-
-  const handleFileUpload = (files: File[]) => {
-    if (files && files.length > 0) {
-      const file = files[0];
-
-      if (!file.name.toLowerCase().endsWith('.zip')) {
-        alert('zip 파일만 업로드 가능합니다.');
-        return;
-      }
-
-      form.setValue('file', files[0]);
-    }
-  };
-
-  return (
-    <>
-      <Stack alignItems='center'>
-        <Typography variant='heading2B' sx={{ color: colors.gray80 }}>
-          실적(EDI) 입력
-        </Typography>
-      </Stack>
-      <Stack direction='row' alignItems='center' sx={{ marginTop: '20px' }}>
-        <Typography variant='largeTextM' sx={{ color: colors.gray80, width: '120px' }}>
-          정산월
-        </Typography>
-        <Controller
-          control={form.control}
-          name={'settlementMonth'}
-          render={({ field }) => (
-            <MedipandaDatePicker
-              {...field}
-              label='정산월'
-              format={DATEFORMAT_YYYY_MM}
-              views={['year', 'month']}
-              sx={{
-                width: '330px',
-                backgroundColor: colors.gray10,
-              }}
-            />
-          )}
-        />
-      </Stack>
-      <Stack direction='row' alignItems='center'>
-        <Typography variant='largeTextM' sx={{ color: colors.gray80, width: '120px' }}>
-          처방월
-        </Typography>
-        <Controller
-          control={form.control}
-          name={'prescriptionMonth'}
-          render={({ field }) => (
-            <MedipandaDatePicker
-              {...field}
-              label='처방월'
-              format={DATEFORMAT_YYYY_MM}
-              views={['year', 'month']}
-              sx={{
-                width: '330px',
-                backgroundColor: colors.gray10,
-              }}
-            />
-          )}
-        />
-      </Stack>
-      <Stack direction='row' alignItems='center'>
-        <Typography variant='largeTextM' sx={{ color: colors.gray80, width: '120px' }}>
-          파일업로드
-        </Typography>
-        <Controller
-          control={form.control}
-          name={'file'}
-          render={({ field }) =>
-            field.value !== null ? (
-              <MedipandaOutlinedInput
-                value={field.value.name}
-                disabled
-                sx={{
-                  flexGrow: 1,
-                  height: '50px',
-                  backgroundColor: colors.gray10,
-                }}
-              />
-            ) : (
-              <MedipandaFileUploadButton onChange={handleFileUpload} sx={{ width: '330px' }} />
-            )
-          }
-        />
-      </Stack>
-      <Typography variant='smallTextR' sx={{ color: 'red', whiteSpace: 'pre-wrap' }}>
-        파일 업로드시 주의사항
-        <br />
-        1. 한번에 업로드시 zip파일로 업로드 해주세요
-        <br />
-        2. zip 파일 내 각 파일명: 딜러명_거래처명_처방월 (ex. 홍길동_메디판다_202504)으로 저장해주세요
-        <br />
-        3. png, jpg, jpeg, pdf파일만 업로드 가능해요
-        <br />
-        4. 파일명내에 처방월이 선택한 처방월과 일치하게 해주세요
-        <br />
-      </Typography>
-      {Object.keys(form.formState.errors).map(key => {
-        return (
-          <Typography key={key} variant='smallTextR' sx={{ color: 'red' }}>
-            {form.formState.errors[key]?.message}
-          </Typography>
-        );
-      })}
-      <Stack
-        direction='row'
-        justifyContent='center'
-        gap='10px'
-        sx={{
-          marginTop: '10px',
-        }}
-      >
-        <MedipandaButton
-          onClick={() => form.reset()}
-          variant='outlined'
-          size='large'
-          color='secondary'
-          sx={{
-            width: '160px',
-          }}
-        >
-          취소
-        </MedipandaButton>
-        <MedipandaButton
-          onClick={form.handleSubmit(submitHandler)}
-          variant='contained'
-          size='large'
-          color='secondary'
-          sx={{
-            width: '160px',
-          }}
-        >
-          등록
-        </MedipandaButton>
-      </Stack>
-
-      <EdiBatchUploadErrorDialog open={uploadErrors.length > 0} onClose={() => setUploadErrors([])} errors={uploadErrors} />
-    </>
-  );
-}
-
-function EdiBatchUploadErrorDialog({ open, onClose, errors }: { open?: boolean; onClose?: () => void; errors: FileValidationErrorDto[] }) {
-  return (
-    <Dialog
-      open={open ?? false}
-      onClose={onClose}
-      sx={{
-        '& .MuiDialog-paper': {
-          width: '600px',
-          borderRadius: '20px',
-        },
-      }}
-    >
-      <DialogTitle
-        sx={{
-          padding: '40px 30px',
-          backgroundColor: colors.gray20,
-        }}
-      >
-        <Typography variant='heading2R' sx={{ color: colors.gray80 }}>
-          업로드한 파일중에 <span style={{ color: colors.red }}>{errors.length}</span>개의 잘못된 파일이 있습니다.
-          <br />
-          확인 후 업로드 부탁드립니다.
-        </Typography>
-        <Stack sx={{ marginTop: '20px' }}>
-          {errors.map(error => (
-            <Typography key={error.message} variant='largeTextR' sx={{ color: colors.red }}>
-              {error.message}
-            </Typography>
-          ))}
-        </Stack>
-      </DialogTitle>
-      <Stack
-        sx={{
-          padding: '40px 30px',
-        }}
-      >
-        <Typography variant='heading1.7B' sx={{ color: colors.gray80 }}>
-          파일명
-        </Typography>
-        <Stack sx={{ marginTop: '20px' }}>
-          {errors.map(error => (
-            <Typography
-              key={error.error}
-              variant='heading2R'
-              sx={{
-                color: colors.red,
-              }}
-            >
-              {error.fileName}
-            </Typography>
-          ))}
-          <MedipandaButton variant='contained' size='large' color='secondary' sx={{ marginTop: '40px' }} onClick={onClose}>
-            닫기
-          </MedipandaButton>
-        </Stack>
-      </Stack>
-    </Dialog>
   );
 }
