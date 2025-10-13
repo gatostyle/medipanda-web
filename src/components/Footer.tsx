9864e0ec0c1f45c3de9615533b8472fb0aa4c088
JoonYoung Lee <kuroneko0441@gmail.com>
2025-10-14 02:27:25 +0900

    푸터 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,18 +24,19 @@ export function Footer() {
       <MaxWidthContainer>
         <Stack
           direction='row'
-          alignItems='center'
+          alignItems='flex-start'
           sx={{
-            height: '150px',
-            paddingY: '20px',
+            height: '260px',
+            paddingTop: '48px',
+            paddingBottom: '72px',
             boxSizing: 'border-box',
           }}
         >
-          <img src='/assets/logo-dark.svg' style={{ height: '34px' }} />
+          <img src='/assets/logo-dark.svg' style={{ width: '184px', height: '34px' }} />
           <Stack sx={{ ml: '104px' }}>
             <Stack direction='row' sx={{ marginBottom: '20px' }}>
               <Link
-                variant='mediumTextB'
+                variant='largeTextB'
                 underline='hover'
                 component={RouterLink}
                 to='/terms'
@@ -45,9 +46,11 @@ export function Footer() {
               >
                 이용약관
               </Link>
-              <span style={{ color: colors.white, margin: '0 8px' }}>|</span>
+              <Typography variant='largeTextM' sx={{ margin: '0 8px' }}>
+                |
+              </Typography>
               <Link
-                variant='mediumTextB'
+                variant='largeTextB'
                 underline='hover'
                 component={RouterLink}
                 to='/privacy'
@@ -58,24 +61,48 @@ export function Footer() {
                 개인정보처리방침
               </Link>
             </Stack>
-            <Typography variant='smallTextR' sx={{ color: colors.white }}>
-              법인명: (주)케이앤메디슨 | 서울특별시 강남구 논현로 416, 지하1층 운기빌딩(역삼동) | 대표 황혁진 | 사업자등록번호 465-86-03299
+            <Typography variant='largeTextR' sx={{ color: colors.gray50 }}>
+              법인명 : (주)케이앤메디슨
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              서울시 강남구 논현로 416, 지하1층 운기빌딩(역삼동)
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              대표 : 황혁진
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              사업자등록번호 : 465-86-03299
               <br />
-              대표전화: 070-7599-2528
+              대표전화 : 070-8666-1102
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              고객센터 : 오전 9시~오후 5시(토/일/공유일 휴무)
               <br />
-              이메일:{' '}
+              통신판매번호 : 제 2025-서울강남-05488 호
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              개인정보관리책임자 : 황혁진
+              <Typography variant='largeTextM' sx={{ margin: '0 8px', color: colors.gray60 }}>
+                |
+              </Typography>
+              메일 :{' '}
               <Link
                 underline='hover'
                 component={RouterLink}
                 to='mailto:info@knmedicine.com'
                 sx={{
-                  color: colors.white,
+                  color: colors.gray50,
                 }}
               >
                 info@knmedicine.com
               </Link>
               <br />
-              통신판매번호: 제 2025-서울강남-05488 호
+              Copyright©knmedicine All Rights Reserved.
             </Typography>
           </Stack>
         </Stack>
